b8bae889e363ec9c8d17629cc18319e9efc04aa2
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-01 15:21:07 +0800

    Update Main.php

--- src/AdvancedRules/Main.php
+++ src/AdvancedRules/Main.php
@@ -1,1 +1,16 @@
+ <?php
 
+namespace Example;
+
+use pocketmine\plugin\PluginBase; 
+
+class Example extends PluginBase{
+}
+public function onLoad(){
+$this->getLogger()->info("onLoad() has been called!");
+}
+public function onEnable(){
+        $this->getLogger()->info("onEnable() has been called!");
+}
+public function onDisable(){ $this->getLogger()->info("onDisable() has been called!");
+}
