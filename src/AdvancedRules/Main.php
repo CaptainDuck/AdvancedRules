6dec3ba47b163ac920e255c6dd824994e5672c26
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-01 16:34:11 +0800

    Update Main.php
    Added on disable/enable console events.

--- src/AdvancedRules/Main.php
+++ src/AdvancedRules/Main.php
@@ -7,10 +7,10 @@ use pocketmine\plugin\PluginBase;
 class AdvancedRules extends PluginBase{
 }
 public function onLoad(){
-$this->getLogger()->info("onLoad() has been called!");
+$this->getLogger()->info("onLoad() AdvancedRules by CaptainDuck has been enabled!");
 }
 public function onEnable(){
-        $this->getLogger()->info("onEnable() has been called!");
+        $this->getLogger()->info("onEnable() AdvancedRules by CaptainDuck has been enabled!");
 }
-public function onDisable(){ $this->getLogger()->info("onDisable() has been called!");
+public function onDisable(){ $this->getLogger()->info("onDisable() AdvancedRules by CaptainDuck has been disabled!");
 }
