91998255b08655818deacde58b7756ab0a75683f
TheDragonRing <tom.vines@rocketmail.com>
2016-05-01 21:19:49 +1000

    Update Main.php

--- src/AdvancedRules/Main.php
+++ src/AdvancedRules/Main.php
@@ -1,16 +1,23 @@
 <?php
 
 namespace AdvancedRules\Main;
-
 use pocketmine\plugin\PluginBase; 
-use AdvancedRules\Rules
+use AdvancedRules\Rules;
+use pocketmine\utils\Config;
 
 class AdvancedRules extends PluginBase{
 
 	public function onEnable(){
-		       $this->getServer()->getLogger()->info(TextFormat::BLUE . "AdvancedRules v1.0.0b by CaptainDuck enabled.");\
+		       $this->getServer()->getLogger()->info(TextFormat::BLUE . "AdvancedRules v1.0.0b by CaptainDuck enabled.");
+                       $this->saveDefaultConfig();
+                       $config = new Config($this->getDataFolder . "rules.yml", Config::YAML);
+                       $config->save();
 	}
 
 	public function onDisable(){
 		       $this->getServer()->getLogger()->info(TextFormat::GRAY . ">" . TextFormat::RED . "RED" . "AdvancedRules v1.0.0b disabled");
 	}
+
+}
+
+?>
