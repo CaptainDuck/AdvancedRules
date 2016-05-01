6af6a50b8c68558baed806cb71ff65d4e0f33bcd
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-01 16:57:37 +0800

    Update Main.php

--- src/AdvancedRules/Main.php
+++ src/AdvancedRules/Main.php
@@ -6,11 +6,13 @@ use pocketmine\plugin\PluginBase;
 
 class AdvancedRules extends PluginBase{
 }
-public function onLoad(){
-$this->getLogger()->info("onLoad() AdvancedRules by CaptainDuck has been enabled!");
-}
-public function onEnable(){
-        $this->getLogger()->info("onEnable() AdvancedRules by CaptainDuck has been enabled!");
-}
-public function onDisable(){ $this->getLogger()->info("onDisable() AdvancedRules by CaptainDuck has been disabled!");
-}
+        public function onLoad(){
+                $this->getLogger()->info("onLoad() AdvancedRules by CaptainDuck has been enabled!");
+        }
+        
+        public function onEnable(){
+                $this->getLogger()->info("onEnable() AdvancedRules by CaptainDuck has been enabled!");
+        }
+        public function onDisable(){ 
+                $this->getLogger()->info("onDisable() AdvancedRules by CaptainDuck has been disabled!");
+        }
