6bee2d6aab8e6bfd8782dc80c10350a276bc23f7
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-05 20:10:57 +0800

    Fixed Commands issue and added more stuff

--- src/AdvancedRules/Rules.php
+++ src/AdvancedRules/Rules.php
@@ -20,32 +20,34 @@ class Rules extends Command implements PluginIdentifiableCommand{
 	
 	}
 	   case player:
+                        $sender->sendMessage("§a§l§o>§7>§cRules for Players:§7<§a<")
 			$sender->sendMessage("#1: Dont use any mods or hacks");
 			$sender->sendMessage("#2: No cussing, be kind.");
-			$sender->sendMessage("#3: Be respectful to any staff or others.");
+			$sender->sendMessage("#3: Be respectful to any staff and others.");
 			$sender->sendMessage("#4: Inappropriate usernames are discouraged..");
 			$sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
 	}
-        public function execute(CommandSender $sender, $lbl, array $args){
-		if(!isset($args[0]) or strtolower($args[0]) === "notes"){
-		$sender->sendMessage("If you disobey the rules repeatedly, you may suffer from any minor/major punishment (based on the disobeyed rules).");
-		$sender->sendMessage("If you found someone doing this, please contact a staff member to handle this.");
-		$sender->sendMessage("All rules in players must be obeyed in all ranks.")
+            case notes:
+                        $sender->sendMessage("§a§l§o>§7>§cNotes for the Rules:§7<§a<")
+		        $sender->sendMessage("If you disobey the rules repeatedly, you may suffer from any minor/major punishment (based on the disobeyed rules).");
+		        $sender->sendMessage("If you found someone doing this, please contact a staff member to handle this.");
+		        $sender->sendMessage("All rules in players must be obeyed in all ranks.")
 		return true;
 		}
-	public function execute(CommandSender $sender, $lbl, array $args){
-		if(!isset($args[0]) or strtolower($args[0]) === "premium"){
+	  case premium:
+                        $sender->sendMessage("§a§l§o>§7>§cRules for Premium Players:§7<§a<")
 			$sender->sendMessage("#1: Dont use any mods or hacks");
 			$sender->sendMessage("#2: No cussing, be kind.");
-			$sender->sendMessage("#3: Be respectful to any staff or others.");
+			$sender->sendMessage("#3: Be respectful to any staff and others.");
 			$sender->sendMessage("#4: Inappropriate usernames are discouraged..");
 			$sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
 		}
-	public function execute(CommandSender $sender, $lbl, array $args){
-		if(!isset($args[0]) or strtolower($args[0]) === "staff"){
+	   case staff:
+                        $sender->sendMessage("§a§l§o>§7>§cRules for Staff Members:§7<§a<")
 			$sender->sendMessage("#1: Dont use any mods or hacks");
 			$sender->sendMessage("#2: No cussing, be kind.");
-			$sender->sendMessage("#3: Be respectful to any staff or others.");
+			$sender->sendMessage("#3: Be respectful to any staff and others.");
 			$sender->sendMessage("#4: Inappropriate usernames are discouraged..");
+                        $sender->sendMessage("#5: Don't Ban or Kick anyone without any appropriate reason.")
 			$sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
 		}
