be506a61643d70ec2e206fd23c363a337a053af5
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-08 20:58:48 +0800

    Finishing Curly Braces later :/

--- src/AdvancedRules/Rules.php
+++ src/AdvancedRules/Rules.php
@@ -42,9 +42,9 @@ class Rules extends Command implements Listener{
 				}
                                 if($args[0] === "notes"){
                                         $sender->sendMessage("§a§l§o>§7>§cNotes for the Rules:§7<§a<");
-		                          $sender->sendMessage("If you disobey the rules repeatedly, you may suffer from any minor/major punishment (based on the disobeyed rules).");
-		                          $sender->sendMessage("If you found someone doing this, please contact a staff member to handle this.");
-		                          $sender->sendMessage("All rules in players must be obeyed in all ranks.");
+		                        $sender->sendMessage("If you disobey the rules repeatedly, you may suffer from any minor/major punishment (based on the disobeyed rules).");
+		                        $sender->sendMessage("If you found someone doing this, please contact a staff member to handle this.");
+		                        $sender->sendMessage("All rules in players must be obeyed in all ranks.");
                                 }  
                                 if($args[0] === "notes"){
                                         $sender->sendMessage("§a§l§o>§7>§cRules for Premium Players:§7<§a<")
@@ -61,6 +61,7 @@ class Rules extends Command implements Listener{
 			                $sender->sendMessage("#3: Be respectful to any staff and others.");
 			                $sender->sendMessage("#4: Inappropriate usernames are discouraged..");
                                         $sender->sendMessage("#5: Don't Ban or Kick anyone without any appropriate reason.")
-			$sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
-	}
-		}
+			                $sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
+                                        }
+	
+		
