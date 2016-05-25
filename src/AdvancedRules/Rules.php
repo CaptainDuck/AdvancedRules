9ed67ff3f8d64d99f6d4e144ce23fd81e8d8f8d8
Harris.com <megaspprt@gmail.com>
2016-05-26 00:30:16 +0800

    #Update : editable rules

--- src/AdvancedRules/Rules.php
+++ src/AdvancedRules/Rules.php
@@ -39,29 +39,29 @@ class Rules extends Command implements Listener{
         	                switch ($args[0]) {
         		                case player:
         		                       $sender->sendMessage("§a§l§o>§7>§cRules for Players:§7<§a<");
-		                               $sender->sendMessage("#1: Dont use any mods or hacks");
-	              	                       $sender->sendMessage("#2: No cussing, be kind.");
-		                               $sender->sendMessage("#3: Be respectful to any staff and others.");
-		                               $sender->sendMessage("#4: Inappropriate usernames are discouraged..");
+		                               $sender->sendMessage("#1:". $this->getConfig()->get("ar1"));
+	              	                       $sender->sendMessage("#2:". $this->getConfig()->get("ar2"));
+		                               $sender->sendMessage("#3:". $this->getConfig()->get("ar3"));
+		                               $sender->sendMessage("#4:". $this->getConfig()->get("ar4"));
 		                               $sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
 		                               return true;
         		                       break;
         		                case premium:
         		                       $sender->sendMessage("§a§l§o>§7>§cRules for Players:§7<§a<");
-		                               $sender->sendMessage("#1: Dont use any mods or hacks");
-		                               $sender->sendMessage("#2: No cussing, be kind.");
-		                               $sender->sendMessage("#3: Be respectful to any staff and others.");
-		                               $sender->sendMessage("#4: Inappropriate usernames are discouraged..");
+		                               $sender->sendMessage("#1:". $this->getConfig()->get("ar5"));
+	              	                       $sender->sendMessage("#2:". $this->getConfig()->get("ar6"));
+		                               $sender->sendMessage("#3:". $this->getConfig()->get("ar7"));
+		                               $sender->sendMessage("#4:". $this->getConfig()->get("ar8"));
 		                               $sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
 		                               return true;
 		                               break;
 		                        case staff:
 		                               $sender->sendMessage("§a§l§o>§7>§cRules for Staff Members:§7<§a<");
-		                               $sender->sendMessage("#1: Dont use any mods or hacks");
-		                               $sender->sendMessage("#2: No cussing, be kind.");
-		                               $sender->sendMessage("#3: Be respectful to any staff and others.");
-		                               $sender->sendMessage("#4: Inappropriate usernames are discouraged..");
-                                               $sender->sendMessage("#5: Don't Ban or Kick anyone without any appropriate reason.");
+		                               $sender->sendMessage("#1:". $this->getConfig()->get("ar9"));
+	              	                       $sender->sendMessage("#2:". $this->getConfig()->get("ar10"));
+		                               $sender->sendMessage("#3:". $this->getConfig()->get("ar11"));
+		                               $sender->sendMessage("#4:". $this->getConfig()->get("ar12"));
+                                               $sender->sendMessage("#5:". $this->getConfig()->get("ar13"));
 	                                       $sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
 	                                       return true;
 	                                       break;
