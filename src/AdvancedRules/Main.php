5bc76fb584008f70b916471d57b40f0c12f96f2b
CaptainDuck <jmdimayuga021@gmail.com>
2016-06-05 17:32:19 +0800

    Update Main.php

--- src/AdvancedRules/Main.php
+++ src/AdvancedRules/Main.php
@@ -22,51 +22,51 @@ class Main extends PluginBase implements Listener{
 	$this->getServer()->getLogger()->info(TextFormat::GRAY . ">" . TextFormat::RED . "RED" . "AdvancedRules v1.0.0b disabled");
     }
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-            switch($cmd->getName()){
-                case "rules":
-                    if($sender->hasPermission("ar.rules")){
-                        $sender->sendMessage(C::RED . "Usage: /rules (player, premium, staff)");
-                        break;
-                	return false;
-                    }
-                if(isset($args[0])){;
-        	    switch($args[0]){
-        		case "player":
-                            if($sender->hasPermission("ar.player")){
-        		        $sender->sendMessage("§a§l§o>§7>§cRules for Players:§7<§a<");
-		                $sender->sendMessage("#1:". $this->getConfig()->get("ar1"));
-	              	        $sender->sendMessage("#2:". $this->getConfig()->get("ar2"));
-		                $sender->sendMessage("#3:". $this->getConfig()->get("ar3"));
-		                $sender->sendMessage("#4:". $this->getConfig()->get("ar4"));
-		                $sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
-		                return true;
-        		        break;
-                            }
-        	        case "premium":
-                            if($sender->hasPermission("ar.premium")){
-        		        $sender->sendMessage("§a§l§o>§7>§cRules for Players:§7<§a<");
-		                $sender->sendMessage("#1:". $this->getConfig()->get("ar5"));
-	              	        $sender->sendMessage("#2:". $this->getConfig()->get("ar6"));
-		                $sender->sendMessage("#3:". $this->getConfig()->get("ar7"));
-		                $sender->sendMessage("#4:". $this->getConfig()->get("ar8"));
-		                $sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
-		                return true;
-		                break;
-                            }
-		        case "staff":
-                            if($sender->hasPermission("ar.staff")){
-		                $sender->sendMessage("§a§l§o>§7>§cRules for Staff Members:§7<§a<");
-		                $sender->sendMessage("#1:". $this->getConfig()->get("ar9"));
-	              	        $sender->sendMessage("#2:". $this->getConfig()->get("ar10"));
-		                $sender->sendMessage("#3:". $this->getConfig()->get("ar11"));
-		                $sender->sendMessage("#4:". $this->getConfig()->get("ar12"));
-                                $sender->sendMessage("#5:". $this->getConfig()->get("ar13"));
-	                        $sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
-	                        return true;
-	                        break;
-                            }
-                    }
+        switch($cmd->getName()){
+            case "rules":
+                if($sender->hasPermission("ar.rules")){
+                    $sender->sendMessage(C::RED . "Usage: /rules (player, premium, staff)");
+                    return true;
+                    break;
+                }
+            if(isset($args[0])){;
+        	switch($args[0]){
+        	    case "player":
+                        if($sender->hasPermission("ar.player")){
+        		    $sender->sendMessage("§a§l§o>§7>§cRules for Players:§7<§a<");
+		            $sender->sendMessage("#1:". $this->getConfig()->get("ar1"));
+	              	    $sender->sendMessage("#2:". $this->getConfig()->get("ar2"));
+		            $sender->sendMessage("#3:". $this->getConfig()->get("ar3"));
+		            $sender->sendMessage("#4:". $this->getConfig()->get("ar4"));
+		            $sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
+		            return true;
+        		    break;
+                        }
+        	    case "premium":
+                        if($sender->hasPermission("ar.premium")){
+        		    $sender->sendMessage("§a§l§o>§7>§cRules for Players:§7<§a<");
+		            $sender->sendMessage("#1:". $this->getConfig()->get("ar5"));
+	              	    $sender->sendMessage("#2:". $this->getConfig()->get("ar6"));
+		            $sender->sendMessage("#3:". $this->getConfig()->get("ar7"));
+		            $sender->sendMessage("#4:". $this->getConfig()->get("ar8"));
+		            $sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
+		            return true;
+		            break;
+                        }
+		    case "staff":
+                        if($sender->hasPermission("ar.staff")){
+		            $sender->sendMessage("§a§l§o>§7>§cRules for Staff Members:§7<§a<");
+		            $sender->sendMessage("#1:". $this->getConfig()->get("ar9"));
+	              	    $sender->sendMessage("#2:". $this->getConfig()->get("ar10"));
+		            $sender->sendMessage("#3:". $this->getConfig()->get("ar11"));
+		            $sender->sendMessage("#4:". $this->getConfig()->get("ar12"));
+                            $sender->sendMessage("#5:". $this->getConfig()->get("ar13"));
+	                    $sender->sendMessage("If you want to learn more do /rules <player, premium, staff, notes>");
+	                    return true;
+	                    break;
+                        }
                 }
             }
+        }
     }
 }
